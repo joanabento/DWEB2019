02cad0fa0ec3584d6236ddd0de23b2ddada2fbd2
joanabento <55669786+joanabento@users.noreply.github.com>
2019-10-28 22:41:36 +0000

    Delete apagar.js

--- apagar.js
+++
@@ -1,6 +0,0 @@
-function ApagaItem(ident){
-    console.log('Vou tentar apagar o' + ident + '...')
-    axios.delete('/' + ident)
-        .then(response => window.location.assign('/'))
-        .catch(error => console.log(error))
-}
